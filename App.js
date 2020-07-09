410534d1e0b007df85b4ab5a61796aa52008f090
Игорь Матвийцев <igmatviytsev@gmail.com>
2020-07-09 14:41:11 +0300

    Add state persistance

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, Linking, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { MyTheme } from './src/utility/theme';
 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
@@ -12,7 +13,39 @@ import fixtureReducer from './src/store/fixture/reducer';
 import playerReducer from './src/store/player/reducer';
 import Layout from './src/components/Layout/Layout';
 
+const PERSISTENCE_KEY = 'NAVIGATION_STATE';
+
 const App = () => {
+  const [isReady, setIsReady] = React.useState(false);
+  const [initialState, setInitialState] = React.useState();
+
+  React.useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (Platform.OS !== 'web' && initialUrl == null) {
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString ? JSON.parse(savedStateString) : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   const store = createStore(combineReducers({
     club: clubReducer,
     fxt: fixtureReducer,
@@ -21,7 +54,13 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <NavigationContainer theme={MyTheme}>
+      <NavigationContainer
+        theme={MyTheme}
+        initialState={initialState}
+        onStateChange={(state) =>
+          AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+        }
+      >
         <SafeAreaView style={styles.mainContainer}>
           <Layout />
         </SafeAreaView>
